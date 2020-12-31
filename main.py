d0526d929766e37871876d4a9be69f1cb0605c9b
Jeyson Calvache <jeison130@gmail.com>
2020-12-31 12:02:42 -0500

    Init

---
+++ main.py
@@ -0,0 +1,18 @@
+import youtube_dl
+
+links = [line.strip() for line in open('input.txt')]
+
+tmpdir = "output"
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '190',
+    }],
+    'outtmpl': './output/%(title)s.%(ext)s',
+}
+
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(links)
